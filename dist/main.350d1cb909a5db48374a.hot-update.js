a9db874bbec5e6932248ccd031eb8d8a94087124
Ahmet Berdiyev <berdiyew707@gmail.com>
2022-12-02 14:23:56 +0300

    initial project && configuration webpack/react

---
+++ dist/main.350d1cb909a5db48374a.hot-update.js
@@ -0,0 +1,36 @@
+"use strict";
+self["webpackHotUpdatepet_game"]("main",{
+
+/***/ "./src/App.tsx":
+/*!*********************!*\
+  !*** ./src/App.tsx ***!
+  \*********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var react_dom_client__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react-dom/client */ "./node_modules/react-dom/client.js");
+/* harmony import */ var _components_HelloWorld__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./components/HelloWorld */ "./src/components/HelloWorld.tsx");
+Object(function webpackMissingModule() { var e = new Error("Cannot find module './components/index'"); e.code = 'MODULE_NOT_FOUND'; throw e; }());
+
+
+
+
+var container = document.getElementById('root');
+var root = (0,react_dom_client__WEBPACK_IMPORTED_MODULE_1__.createRoot)(container);
+root.render(react__WEBPACK_IMPORTED_MODULE_0___default().createElement(_components_HelloWorld__WEBPACK_IMPORTED_MODULE_2__["default"], null));
+
+
+/***/ })
+
+},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ /* webpack/runtime/getFullHash */
+/******/ (() => {
+/******/ 	__webpack_require__.h = () => ("5465362c7c2baffb3eda")
+/******/ })();
+/******/ 
+/******/ }
+);
+//# sourceMappingURL=main.350d1cb909a5db48374a.hot-update.js.map
---
+++ dist/main.770c599676b28a16d519.hot-update.js
@@ -0,0 +1,36 @@
+"use strict";
+self["webpackHotUpdatepet_game"]("main",{
+
+/***/ "./src/App.tsx":
+/*!*********************!*\
+  !*** ./src/App.tsx ***!
+  \*********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var react_dom_client__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react-dom/client */ "./node_modules/react-dom/client.js");
+/* harmony import */ var _components_HelloWorld__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./components/HelloWorld */ "./src/components/HelloWorld.tsx");
+Object(function webpackMissingModule() { var e = new Error("Cannot find module './components/index'"); e.code = 'MODULE_NOT_FOUND'; throw e; }());
+
+
+
+
+var container = document.getElementById('root');
+var root = (0,react_dom_client__WEBPACK_IMPORTED_MODULE_1__.createRoot)(container);
+root.render(react__WEBPACK_IMPORTED_MODULE_0___default().createElement(_components_HelloWorld__WEBPACK_IMPORTED_MODULE_2__["default"], null));
+
+
+/***/ })
+
+},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ /* webpack/runtime/getFullHash */
+/******/ (() => {
+/******/ 	__webpack_require__.h = () => ("7b9bf67c6fae88c3a264")
+/******/ })();
+/******/ 
+/******/ }
+);
+//# sourceMappingURL=main.770c599676b28a16d519.hot-update.js.map
